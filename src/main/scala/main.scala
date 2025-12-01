32853e958578bfe7fb35354b5821876ca367be55
ottorohenkohl <message@rohenkohl.dev>
2025-12-01 11:31:57 +0100

    day 1: riddle 1

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,7 +1,23 @@
-import types.grid.Grid
+import Direction.{Left, Right}
 
 @main
 def main(): Unit =
-  val input: Grid = os.read(os.pwd / "input")
+  given Conversion[Char, Direction] with
+    def apply(char: Char): Direction = char match
+      case 'L' => Left
+      case 'R' => Right
 
-  println(input.mkString)
+  val rotations = os.read.lines(os.pwd / "input").map(line => Rotation(line.charAt(0), line.splitAt(1)(1).toInt))
+  val dials = rotations.foldLeft(List(Dial(50))) { (dials, rotation) => dials :+ dials.last.rotate(rotation) }
+
+  println(dials.count(_.position == 0))
+
+case class Rotation(direction: Direction, amount: Int)
+case class Dial(position: Int):
+  def rotate(rotation: Rotation): Dial = rotation.direction match
+    case Left => Dial((position + (100 - rotation.amount)) % 100)
+    case Right => Dial((position + rotation.amount) % 100)
+
+enum Direction:
+  case Left
+  case Right
