337f409d37d098c84ae47eb79abd4581661ac277
ottorohenkohl <message@rohenkohl.dev>
2025-12-02 13:15:54 +0100

    day 2: riddle 1

--- source/src/main/scala/main.scala
+++ source/src/main/scala/main.scala
@@ -1,5 +1,11 @@
 package dev.rohenkohl
 package advent
 
+val rangePattern = "(\\d+)-(\\d+)".r
+val duplicationPattern = "(\\d+)\\1".r
+val inputText = os.read(os.pwd / "input")
+
 @main
-def main(): Unit = ???
+def main(): Unit = println(rangePattern.findAllMatchIn(inputText)
+  .map(found => found.group(1).toLong to found.group(2).toLong).fold(Seq())(_ ++ _)
+  .filter(_.toString.matches(duplicationPattern.toString())).sum)
