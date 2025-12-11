3fc03ef16368a7a87f6fda459cbfa3a17bcb51b8
ottorohenkohl <message@rohenkohl.dev>
2025-12-11 12:38:44 +0100

    day 5: part 2

--- source/src/main/scala/main.scala
+++ source/src/main/scala/main.scala
@@ -3,16 +3,23 @@ package advent
 
 case class Range(lower: Long, upper: Long):
   def contains(long: Long) = lower <= long && long <= upper
+  def elements() = upper - lower + 1
 
 @main
 def main() =
-  val input = os.read.lines(os.pwd / "input.aoc")
-
-  val parsedIds = input.dropWhile(_.nonEmpty).tail.map(_.toLong)
-  val parsedRanges = input
+  val combinedRanges = os.read
+    .lines(os.pwd / "input.aoc")
     .takeWhile(_.nonEmpty)
     .map("(\\d+)-(\\d+)".r.findFirstMatchIn(_).get)
     .map(_.subgroups)
     .map(matched => Range(matched(0).toLong, matched(1).toLong))
+    .sortBy(_.lower)
+    .foldLeft(List[Range]()) { (accumulator, range) =>
+      accumulator match
+        case head :: tail =>
+          if head.upper < range.lower then range :: accumulator
+          else Range(head.lower, (head.upper, range.upper).toList.max) :: tail
+        case _ => List(range)
+    }
 
-  println(parsedIds.count(id => parsedRanges.exists(_.contains(id))))
+  println(combinedRanges.map(_.elements()).sum)
